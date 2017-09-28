b3a884360cec5aed893f5d8219b9a21945358b0a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-28 20:35:34 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,8 +11,12 @@ class School
   end
 
   def add_student(name, grade)
-    @roster[grade] = []
-    @roster[grade] << name
+    if @roster.keys.include?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
   end
 end
 
